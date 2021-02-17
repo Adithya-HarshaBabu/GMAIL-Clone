093d1eb76f5540bc0039b337ed2a78c6bd1d6f35
Adithya Harsha Babu <ahb437@nyu.edu>
2021-02-17 16:15:48 -0500

    sidebar

--- src/Components/layout/Sidebar.js
+++ src/Components/layout/Sidebar.js
@@ -1,14 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import Compose from "../Buttons/Compose";
-
+import { sidebarButtonItems } from "../../Data/SidebarButtonItems";
 const Sidebar = () => {
   return (
     <Wrapper>
       <ComposeWrapper>
         <Compose />
       </ComposeWrapper>
-      <SideButtonsWrapper>{/* All the Buttons */}</SideButtonsWrapper>
+      <SideButtonsWrapper>
+        {sidebarButtonItems.map((item) => (
+          <SidebarButtonItem>
+            {" "}
+            {item.icon} {item.text}{" "}
+          </SidebarButtonItem>
+        ))}
+      </SideButtonsWrapper>
       <MeetWrapper>{/* Google Meet */}</MeetWrapper>
       <HangoutsWrapper>{/* Hangouts */}</HangoutsWrapper>
       <IconsWrapper>{/* Bottom Icons */}</IconsWrapper>
@@ -18,9 +25,29 @@ const Sidebar = () => {
 
 export default Sidebar;
 
-const Wrapper = styled.div``;
-const ComposeWrapper = styled.div``;
+const Wrapper = styled.div`
+  border-right: 1px solid lightgrey;
+  height: 100vh;
+`;
+const ComposeWrapper = styled.div`
+  display: grid;
+  place-items: start;
+  padding: 10px 20px;
+`;
 const SideButtonsWrapper = styled.div``;
 const MeetWrapper = styled.div``;
 const HangoutsWrapper = styled.div``;
 const IconsWrapper = styled.div``;
+const SidebarButtonItem = styled.div`
+  display: grid;
+  grid-template-columns: 14% auto;
+  color: grey;
+  padding: 5px 25px;
+  border-radius: 0 100px 100px 0;
+  margin-right: 4px;
+
+  :hover {
+    background-color: #f5f7f7;
+    cursor: pointer;
+  }
+`;
